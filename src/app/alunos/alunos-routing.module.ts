a6ae224e948c82641a4cdd6ae46de3686bd49758
tiagorv <tiago.valerio@betha.com.br>
2021-11-21 16:06:01 -0300

    INicio Projeto

---
+++ src/app/alunos/alunos-routing.module.ts
@@ -0,0 +1,16 @@
+import { NgModule } from '@angular/core';
+import { Routes, RouterModule } from '@angular/router';
+import { AlunosFormComponent } from './alunos-form/alunos-form.component';
+
+
+const routes: Routes = [
+  { path: 'alunosForm', component: AlunosFormComponent}
+
+
+];
+
+@NgModule({
+  imports: [RouterModule.forChild(routes)],
+  exports: [RouterModule]
+})
+export class AlunosRoutingModule { }
