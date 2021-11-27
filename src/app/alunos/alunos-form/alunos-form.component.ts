32f4ab754590b468bf05da86e6b549eb6cf40920
tiagorv <tiago.valerio@betha.com.br>
2021-11-27 12:40:23 -0300

    ´[p´[´

--- src/app/alunos/alunos-form/alunos-form.component.ts
+++ src/app/alunos/alunos-form/alunos-form.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Params, Router } from '@angular/router';
+import { Observable } from 'rxjs';
+import { AlunosService } from 'src/app/alunos.service';
+import { Aluno } from '../aluno';
 
 @Component({
   selector: 'app-alunos-form',
@@ -7,9 +11,62 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AlunosFormComponent implements OnInit {
 
-  constructor() { }
+  aluno: Aluno;
+  sucesso: boolean = false;
+  errosApi: String[];
+  id: number;
+
+  constructor(private servicoDeAlunos: AlunosService,
+              private minhaRota: Router,
+              private rotaPassada: ActivatedRoute) {
+      this.aluno = new Aluno();
+   }
 
   ngOnInit(): void {
+      let params : Observable<Params> = this.rotaPassada.params;
+
+      params
+        .subscribe(urlParams => {
+              this.id = urlParams['id'];
+              if (this.id){
+                  this.servicoDeAlunos
+                      .getAlunoById(this.id)
+                      .subscribe(respostaSucesso => {
+                          this.aluno = respostaSucesso;
+                      }, respostaComErro => {
+                          this.aluno = new Aluno();
+                      })
+              }
+        });
+  }
+
+  gravarAluno(){
+      if (this.id){
+            this.servicoDeAlunos
+                .atualizar(this.aluno)
+                .subscribe( respostaComSucesso => {
+                      this.sucesso = true;
+                      this.errosApi = null;
+                }, respostaComErro => {
+                      this.sucesso = false;
+                      this.errosApi = respostaComErro.error.erros;
+                })
+      }else{
+          this.servicoDeAlunos
+              .salvar(this.aluno)
+              .subscribe( respostaComSucesso => {
+                    this.sucesso = true;
+                    this.errosApi = null;
+                    this.aluno = respostaComSucesso;
+              }, respostaComErro => {
+                    this.sucesso = false;
+                    this.errosApi = respostaComErro.error.erros;
+              })
+        }
+  }
+
+  voltarParaListagem(){
+      this.minhaRota.navigate(['/alunosLista']);
   }
 
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { HttpClientModule} from '@angular/common/http';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -7,6 +8,7 @@ import { AppComponent } from './app.component';
 import { TemplateModule } from './template/template.module';
 import { HomeComponent } from './home/home.component'
 import { AlunosModule } from './alunos/alunos.module';
+import { AlunosService } from './alunos.service';
 
 @NgModule({
   declarations: [
@@ -15,11 +17,14 @@ import { AlunosModule } from './alunos/alunos.module';
   ],
   imports: [
     BrowserModule,
+    HttpClientModule,
     AppRoutingModule,
     TemplateModule,
     AlunosModule
   ],
-  providers: [],
+  providers: [
+    AlunosService
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
